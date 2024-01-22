837a624f86a827bc744b47d94571016370500527
ooosj <tmdwn8775@gmail.com>
2024-01-22 23:37:30 +0900

    project 생성

---
+++ src/main/java/backend/sammaru/SammaruApplication.java
@@ -0,0 +1,13 @@
+package backend.sammaru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SammaruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SammaruApplication.class, args);
+	}
+
+}
---
+++ src/test/java/backend/sammaru/SammaruApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.sammaru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SammaruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
